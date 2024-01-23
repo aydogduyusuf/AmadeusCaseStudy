6bb3d2f2b52da2f5c5711ac9810cc96d996aaa49
aydogduyusuf <yusufaydogdu23@gmail.com>
2024-01-23 09:59:48 +0300

    spring initialize

---
+++ src/test/java/com/Amadeus/flightSearch/FlightSearchApplicationTests.java
@@ -0,0 +1,13 @@
+package com.Amadeus.flightSearch;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FlightSearchApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
